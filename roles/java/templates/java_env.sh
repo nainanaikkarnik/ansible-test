b50dc9c8d4d4d5eaa6e44e3a6089c99c1f06f22b
Naina Karnik <naina.karnik@fdj-gs.com>
2020-02-29 11:31:56 +0000

    changes

---
+++ roles/java/templates/java_env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME={{ default_java_home }}
+export JDK_HOME={{ default_java_home }}
+export PATH=$JAVA_HOME/bin:$PATH
---
+++ roles/zookeeper/templates/zookeeper-env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export ZOO_LOG_DIR={{ zookeeper.log_dir }}
